d7ed93f029e2e09e2fef69335f03a75aa7c6d4f4
Egor Rudakov <ke.kupkup44@mail.ru>
2022-02-14 19:52:03 +0300

    UI_Calculator

--- js/hello.js
+++ js/hello.js
@@ -1,1 +1,20 @@
-console.log('Привет');
+function calc(operation, a, b) {
+   if (operation == null || typeof a !== 'number' || typeof b !== 'number') {
+      return 'Error';
+   } else if (operation == 'divis' && b == 0) {
+      return "Can't divide by zero";
+   } 
+   const operations = {
+      sum: a + b,
+      sub: a - b,
+      multi: a * b,
+      divis: a / b,
+      remain: a % b,
+      erect: a ** b, 
+   }
+   if(operation in operations) {
+      return operations[operation];
+   } return 'unknown operation';
+} 
+
+console.log(calc('summ', 2, 2));
